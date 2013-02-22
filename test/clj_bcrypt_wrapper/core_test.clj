e279320753290afc04608acb4d1676ff3207786e
Robert Beaupre <robert@robear.ca>
2013-02-22 15:13:00 -0800

    Initial commit.

--- test/clj_bcrypt_wrapper/core_test.clj
+++ test/clj_bcrypt_wrapper/core_test.clj
@@ -3,5 +3,5 @@
         clj-bcrypt-wrapper.core))
 
 (deftest compare-hashed-to-plain
-  (testing "Ensure a hashed password is equal to itself ha"
+  (testing "Ensure a hashed password is equal to itself."
     (is (= true (check-password "1234567890" (encrypt "1234567890"))))))
